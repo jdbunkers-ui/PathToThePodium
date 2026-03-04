9a2137e0c638d2631be9b849a381cc22ca5b0d96
jdbunkers-ui <j.d.bunkers@gmail.com>
2026-03-04 09:36:26 -0500

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -5,3 +5,16 @@ function getQueryParam(name) {
   const params = new URLSearchParams(window.location.search);
   return params.get(name);
 }
+
+function requireParam(name) {
+  const value = getQueryParam(name);
+
+  if (!value) {
+    console.error(`Missing required parameter: ${name}`);
+    document.getElementById("page-root").innerHTML =
+      `<p>Missing required parameter: ${name}</p>`;
+    throw new Error(`Missing parameter ${name}`);
+  }
+
+  return value;
+}
