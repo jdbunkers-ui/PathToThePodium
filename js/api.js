fe1b3b788a0dc720796c0146bd1420f735022799
jdbunkers-ui <j.d.bunkers@gmail.com>
2026-03-05 07:15:35 -0500

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,7 +1,7 @@
 // /js/api.js
 
 const SUPABASE_URL = "https://ddmulriubdluavwcpyrl.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRkbXVscml1YmRsdWF2d2NweXJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIyMTc2NjUsImV4cCI6MjA4Nzc5MzY2NX0.SuDATgshWSTFjpQkxTWEBMXBspKYvM9QUNCbZuObu60";
+const SUPABASE_ANON_KEY = "YOUR_KEY";
 
 const db = window.supabase.createClient(
   SUPABASE_URL,
@@ -22,7 +22,6 @@ async function testSupabaseConnection() {
 
 }
 
-// RUN THE TEST
 testSupabaseConnection();
 
 
@@ -35,14 +34,12 @@ async function queryView(viewName, filters = {}, orderBy = null) {
     .from(viewName)
     .select("*");
 
-  // Apply filters
   Object.entries(filters).forEach(([column, value]) => {
     if (value !== null && value !== undefined) {
       query = query.eq(column, value);
     }
   });
 
-  // Apply ordering
   if (orderBy) {
     query = query.order(orderBy.column, { ascending: orderBy.ascending ?? true });
   }
