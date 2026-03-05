26321e9c7a7747bd79cde4ae80d27eb8c92f596d
jdbunkers-ui <j.d.bunkers@gmail.com>
2026-03-05 08:16:09 -0500

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,7 +1,7 @@
 // /js/api.js
 
 const SUPABASE_URL = "https://ddmulriubdluavwcpyrl.supabase.co";
-const SUPABASE_ANON_KEY = "YOUR_KEY";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRkbXVscml1YmRsdWF2d2NweXJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIyMTc2NjUsImV4cCI6MjA4Nzc5MzY2NX0.SuDATgshWSTFjpQkxTWEBMXBspKYvM9QUNCbZuObu60";
 
 const db = window.supabase.createClient(
   SUPABASE_URL,
